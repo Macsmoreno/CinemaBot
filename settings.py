16b68a257a6c025ec859ca45a278d9871f1cb222
Irina <murlucien@gmail.com>
2019-03-20 14:13:14 +0300

    Menu

--- settings.py
+++
@@ -1,3 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-API_KEY = '762229804:AAHany0xQdNgBITfwKDtEgvYZ5SJQxUJhmg'
