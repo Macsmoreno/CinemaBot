3f08f058817d326ad1cf645c814bcb34ca499cf3
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-01 23:12:09 +0300

    Добавлены циклы записи парметров в базу данных

--- cinema_info_insert.py
+++ cinema_info_insert.py
@@ -1,24 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_declarative import About_cinema, Base
+from sqlalchemy_declarative import Movies, Base
+from cinema_list_request import movies_request, movie_id_request, movie_trailer
 
-from cinema_list_request import result_of_request
-
-
-engine = create_engine('sqlite:///cinema_information.db')
-
+engine = create_engine('sqlite:///movies.db')
 
 Base.metadata.bind = engine
 
-
 DBSession = sessionmaker(bind=engine)
-
 
 session = DBSession()
 
-
-for j in result_of_request['results']:   
-    instance = About_cinema(title = j['title'], cinema_id = j['id'])
+for j in movies_request['results']:   
+    instance = Movies(title = j['title'], cinema_id = j['id'])
     session.add(instance)
     session.commit()
+
+for i in movie_id_request:
+    instance = Movies(description = i['body_text'] )
+    session.add(instance)
+    session.commit()
+
+for x in movie_id_request:
+    instance = Movies(trailer_url = x['trailer'])
+    session.add(instance)
+    session.commit()
+
