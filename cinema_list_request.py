6a48c3b2912b0b313079cbb9a0ca57670f6412cd
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-01 23:12:48 +0300

    Создан цикл для запроса данных

--- cinema_list_request.py
+++ cinema_list_request.py
@@ -1,18 +1,29 @@
-import requests
-import json
 import datetime
+import json
+import pprint
 
+import requests
 
 TODAY = datetime.datetime.today()
 
 
 movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id,title&page_size=20&actual_since={TODAY}&location=kzn')
 
-
 movies_request = movies.json()
 
+#for names in movies_request['results']:
+#    with open ('List_of_cinema.txt', 'a', encoding='utf-8') as new_file:
+#       new_file.write("{}, id = /{} \n".format(names['title'], str(names['id'])))
 
-for names in movies_request['results']:
-    with open ('List_of_cinema.txt', 'a', encoding='utf-8') as new_file:
-        new_file.write("{}, id = /{} \n".format(names['title'], str(names['id'])))
 
+pp = pprint.PrettyPrinter(indent=4)
+
+for movie in movies_request['results']:
+        movie_id = movie['id']
+        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=&expand=')
+        movie_id_request = movies_id_info.json()
+        movie_description_raw = movie_id_request['body_text'].replace('<p>', '')
+        movie_description = movie_description_raw.replace('</p>', '')
+        movie_trailer = movie_id_request['trailer']
+        #pp.pprint(movie_description)
+        #pp.pprint(movie_trailer)
