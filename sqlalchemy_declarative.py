8008d82df448e30a0ce9ee0032963c3ce98ebf8d
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-01 23:13:53 +0300

    Убранна не нужная таблица, дополнена Movies

--- sqlalchemy_declarative.py
+++ sqlalchemy_declarative.py
@@ -9,20 +9,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class About_cinema(Base):
-    __tablename__ = 'Information'
+class Movies(Base):
+    __tablename__ = 'Movies'
     title = Column(Text, nullable = False)
     cinema_id = Column(Integer, primary_key = True)
+    description = Column(String, nullable = False)
+    trailer_url = Column(String, nullable = True)
 
 
-class Id_info(Base):
-    __tablename__ = 'Id_info'
-    description = Column(Text, nullable = False)
-    trailer_url = Column(String, nullable = False)
-    cinema = relationship(About_cinema)
-
-
-engine = create_engine('sqlite:///cinema_information.db')
+engine = create_engine('sqlite:///movies.db')
 
 
 Base.metadata.create_all(engine)
