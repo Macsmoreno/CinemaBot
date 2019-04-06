2296f1cd3d5cb2e0df2a591cc317d07ce90858be
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-06 18:45:37 +0300

    Изменена работа запроса, добавлена функция result

--- cinema_list_request.py
+++ cinema_list_request.py
@@ -7,23 +7,21 @@ import requests
 TODAY = datetime.datetime.today()
 
 
-movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id,title&page_size=20&actual_since={TODAY}&location=kzn')
+movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id&page_size=100&actual_since={TODAY}&location=kzn')
 
 movies_request = movies.json()
 
-#for names in movies_request['results']:
-#    with open ('List_of_cinema.txt', 'a', encoding='utf-8') as new_file:
-#       new_file.write("{}, id = /{} \n".format(names['title'], str(names['id'])))
-
-
 pp = pprint.PrettyPrinter(indent=4)
+
+pp.pprint(movies_request)
+
+def result(movie_id):
+        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,title,body_text,running_time,trailer')
+        movie_id_request = movies_id_info.json()
+        movie_list = [movie_id_request['id'], movie_id_request['title'], ((movie_id_request['body_text'].replace('<p>', '')).replace('</p>', '')), movie_id_request['trailer']]
+        return movie_list
 
 for movie in movies_request['results']:
         movie_id = movie['id']
-        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=&expand=')
-        movie_id_request = movies_id_info.json()
-        movie_description_raw = movie_id_request['body_text'].replace('<p>', '')
-        movie_description = movie_description_raw.replace('</p>', '')
-        movie_trailer = movie_id_request['trailer']
-        #pp.pprint(movie_description)
-        #pp.pprint(movie_trailer)
+        movie_info = result(movie_id)
+        print(movie_info)
