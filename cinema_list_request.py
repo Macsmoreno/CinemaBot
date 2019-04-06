827857eebc87350c6ab01b908c9dcb35c01be678
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-06 19:51:23 +0300

    добавлена функция

--- cinema_list_request.py
+++ cinema_list_request.py
@@ -1,7 +1,5 @@
 import datetime
 import json
-import pprint
-
 import requests
 
 TODAY = datetime.datetime.today()
@@ -11,17 +9,13 @@ movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id&pag
 
 movies_request = movies.json()
 
-pp = pprint.PrettyPrinter(indent=4)
-
-pp.pprint(movies_request)
 
 def result(movie_id):
-        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,title,body_text,running_time,trailer')
+        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,title,body_text,running_time,trailer&text_format=text')
         movie_id_request = movies_id_info.json()
-        movie_list = [movie_id_request['id'], movie_id_request['title'], ((movie_id_request['body_text'].replace('<p>', '')).replace('</p>', '')), movie_id_request['trailer']]
+        movie_list = [movie_id_request['id'], movie_id_request['title'], movie_id_request['body_text'], movie_id_request['trailer']]
         return movie_list
 
 for movie in movies_request['results']:
         movie_id = movie['id']
         movie_info = result(movie_id)
-        print(movie_info)
