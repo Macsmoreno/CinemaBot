b34702add6de564bf4ca490582f9f9dfca626886
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-06 19:51:53 +0300

    Изменена запись в базу данных

--- cinema_info_insert.py
+++ cinema_info_insert.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy_declarative import Movies, Base
-from cinema_list_request import movies_request, movie_id_request, movie_trailer
+from cinema_list_request import movies_request, result
 
 engine = create_engine('sqlite:///movies.db')
 
@@ -12,8 +12,10 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-for j in movies_request['results']:   
-    instance = Movies(title = j['title'], id = j['id'])
+for movie in movies_request['results']: 
+    movie_id = movie['id']
+    movie_info = result(movie_id)
+    instance = Movies(id = movie['id'], id_of_movie = movie_info[0], title = movie_info[1], description = movie_info[2], trailer_url = movie_info[3])
     session.add(instance)
     session.commit()
 
