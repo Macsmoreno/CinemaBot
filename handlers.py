210967983027cf56f9a0d3259c6da003b7c03501
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-08 21:48:14 +0300

    Добавлен вывод из базы

--- handlers.py
+++ handlers.py
@@ -1,9 +1,22 @@
+from sqlalchemy_declarative import Movies, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///movies.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
+
+
 def cinema_list(bot, update, user_data):
-    with open ('List_of_cinema.txt', 'r', encoding='utf-8') as f:
-        content = f.read()
-        update.message.reply_text(content)
-
-
+        for instance in session.query(Movies).order_by(Movies.id):
+                list_of_movies = []
+                title = instance.title
+                id_of_movies = instance.id_of_movie
+                list_of_movies.extend(f"{title}, /id = {id_of_movies}")
+        update.message.reply_text(list_of_movies)        
 
 def cinema_location(bot, update, user_data):
     pass
