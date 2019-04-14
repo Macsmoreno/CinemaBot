8cff86e5d6f69ae81c424beca8f08402efa3f90d
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-14 21:20:40 +0300

    доработаны cinema_list и cinema_get_id

--- handlers.py
+++ handlers.py
@@ -1,5 +1,10 @@
+from telegram import ReplyKeyboardRemove, ParseMode
+
+
+from query_example import movie_list
+
 from sqlalchemy_declarative import Movies, Base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///movies.db')
@@ -9,14 +14,26 @@ DBSession = sessionmaker()
 DBSession.bind = engine
 session = DBSession()
 
-
 def cinema_list(bot, update, user_data):
-        for instance in session.query(Movies).order_by(Movies.id):
-                list_of_movies = []
-                title = instance.title
-                id_of_movies = instance.id_of_movie
-                list_of_movies.extend(f"{title}, /id = {id_of_movies}")
-        update.message.reply_text(list_of_movies)        
+    update.message.reply_text(f"{movie_list[0:9]}")
+    return "id" 
+
+def cinema_get_id(bot, update, user_data):
+    cinema_id = int(update.message.text.split('id'))
+    row = session.query(Movies).filter(Movies.id == cinema_id).first()
+    text = f"""
+<b>Название</b>: {row.title}
+<b>Описание</b>:
+{row.description}
+    <b>Трейлер</b>:{row.trailer_url}
+    """
+    update.message.reply_text(text, parse_mode = ParseMode.HTML)
+
+
+
+
+
+
 
 def cinema_location(bot, update, user_data):
     pass
