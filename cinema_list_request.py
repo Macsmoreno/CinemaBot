bf04e9750bc485b65a157609ae70fa5a596a5a20
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-14 21:18:39 +0300

    добавлен publication_date в функцию

--- cinema_list_request.py
+++ cinema_list_request.py
@@ -1,19 +1,26 @@
 import datetime
 import json
 import requests
+import pprint
 
-TODAY = datetime.datetime.today()
+pp = pprint.PrettyPrinter(indent=4)
 
 
-movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id&page_size=100&actual_since={TODAY}&location=kzn')
+start_date = datetime.datetime.today()
+
+
+movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id&page_size=100&actual_since={start_date}&location=kzn&imdb_rating')
 
 movies_request = movies.json()
 
-
 def result(movie_id):
-        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,title,body_text,running_time,trailer&text_format=text')
+        movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,publication_date,title,body_text,running_time,trailer&text_format=text&imdb_rating')
         movie_id_request = movies_id_info.json()
-        movie_list = [movie_id_request['id'], movie_id_request['title'], movie_id_request['body_text'], movie_id_request['trailer']]
+        movie_list = [movie_id_request['id'], 
+                movie_id_request['title'], 
+                movie_id_request['body_text'], 
+                movie_id_request['trailer'],
+                movie_id_request['publication_date']]
         return movie_list
 
 for movie in movies_request['results']:
