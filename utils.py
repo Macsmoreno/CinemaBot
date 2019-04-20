98a703849c7a74b2af77e2afb107d875984eadb0
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-20 12:34:37 +0300

    greet_user перенесен

--- utils.py
+++ utils.py
@@ -4,13 +4,6 @@ import settings
 def get_keyboard():
     my_keyboard = ReplyKeyboardMarkup([
                                        ['Сейчас в кино'],
-                                       ['Кинотеатры', 'Избранное']
+                                       ['Кинотеатры']
                                      ], resize_keyboard=True)
     return my_keyboard
-
-
-def greet_user(bot, update, user_data):
-    text = 'Привет, {}!'.format(update.message.chat.first_name)
-    
-    update.message.reply_text(text, reply_markup=get_keyboard())
-    
