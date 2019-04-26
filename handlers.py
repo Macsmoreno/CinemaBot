e8ee732a03f6f83311f676466694b6d59172600a
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-26 16:32:52 +0300

    добавлены функции

--- handlers.py
+++ handlers.py
@@ -1,6 +1,8 @@
 from telegram import MessageEntity, ReplyKeyboardMarkup, \
     ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode, error
-from utils import get_keyboard
+from utils import get_keyboard, list_keyboard
+
+from cinema_list_request import movies_places_id
 
 import logging
 
@@ -30,25 +32,38 @@ def cinema_list(bot, update, user_data):
         kbd_markup = InlineKeyboardMarkup(inlinekbd)
         update.message.reply_text(title, reply_markup = kbd_markup)
 
-def inline_button_pressed(bot, update):
-    print(update.callback_query)
+def inline_button_info(bot, update):
+    query = update.callback_query
+    cinema_id = int(query.data)
+    row = session.query(Movies).filter(Movies.id == cinema_id).first()
+    logging.info(cinema_id)
+    text = f"""
+        ***Название***: {row.title}
+        ***Описание***:
+        {row.description}
+            [Трейлер]:({row.trailer_url})
+            """
+    inline_list_kbd = [[InlineKeyboardButton("Посмотерть сеансы", callback_data = cinema_id)]]
+    logging.info(cinema_id)
+    kbd_list_markup = InlineKeyboardMarkup(inline_list_kbd)
+    query.edit_message_text(text, reply_markup = kbd_list_markup)
+
+def need_more_movies():
+    pass
+
+def back_to_movies():
+    pass
+
+def inline_button_list(bot, update):
     query = update.callback_query
     print(query)
-    cinema_id = int(query.data)
-    print(cinema_id)
-    row = session.query(Movies).filter(Movies.id == cinema_id).first()
-    logging.info("cinema_id")
-    text = f"""
-    <b>Название</b>: {row.title}
-    <b>Описание</b>:
-    {row.description}
-        <b>Трейлер</b>:{row.trailer_url}
-        """
-    update.message.reply_text(text, parse_mode = ParseMode.HTML)
+    callback_id = int(query.data)
+    cinema_list =f'''
+    {movies_places_id(callback_id)}'''
+    query.edit_message_text(cinema_list)
+    
 
 def cinema_location(bot, update, user_data):
     pass
 
 
-def chosen_cinema(bot, update, user_data):
-    pass
