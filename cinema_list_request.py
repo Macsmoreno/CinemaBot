67c7f5442206ecc22d2b51c728a5c987cd91af01
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-26 16:32:12 +0300

    добавлена функция movies_places_id, запрос places

--- cinema_list_request.py
+++ cinema_list_request.py
@@ -5,7 +5,7 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-
+''' дата начала '''
 start_date = datetime.datetime.today()
 
 
@@ -14,6 +14,7 @@ movies = requests.get(f'https://kudago.com/public-api/v1.4/movies/?fields=id&pag
 movies_request = movies.json()
 
 def result(movie_id):
+        ''' собирает инфу про кино по id'''
         movies_id_info = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/?fields=id,publication_date,title,body_text,running_time,trailer&text_format=text&imdb_rating')
         movie_id_request = movies_id_info.json()
         movie_list = [movie_id_request['id'], 
@@ -27,4 +28,22 @@ for movie in movies_request['results']:
         movie_id = movie['id']
         movie_info = result(movie_id)
 
-movies_place = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/showings/?fields=id,movie,place,datetime,price&order_by=price&location=kzn&actual_since={start_date}')
+def movies_places_id(movie_id):
+        '''показывает где идет кино по id'''
+        movies_places_request = requests.get(f'https://kudago.com/public-api/v1.4/movies/{movie_id}/showings/?fields=id,title,movie,place,datetime,price&location=kzn&actual_since={start_date}')
+        movies_places = movies_places_request.json()
+        for info in movies_places['results']: 
+                [
+                info['datetime'],
+                info['movie']['id'],
+                info['place']['id'],
+                info['price']
+                ]
+        return info
+
+''' список кинотеатров '''
+places_request = requests.get("https://kudago.com/public-api/v1.4/places/?fields=id,title,address,foreign_url&order_by=address&text_format=text&location=kzn&categories=cinema")
+
+places = places_request.json()
+
+
