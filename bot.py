c3c822c888ebcd8bf6acf979a9103e48585d2eb1
Macs Moreno <m.m0ren0@yandex.ru>
2019-04-26 16:30:30 +0300

    добавлены inline кнопки

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
-from handlers import greet_user, cinema_list, cinema_location, chosen_cinema, inline_button_pressed
+from handlers import greet_user, cinema_list, cinema_location, inline_button_info, inline_button_list, \
+    need_more_movies, back_to_movies
 
 from telegram.ext import Updater, CommandHandler,  MessageHandler, RegexHandler, ConversationHandler, Filters, \
     CallbackQueryHandler
@@ -27,9 +28,12 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Сейчас в кино)$', cinema_list, pass_user_data=True))
-    dp.add_handler(CallbackQueryHandler(inline_button_pressed))
+    dp.add_handler(CallbackQueryHandler(inline_button_info))
+    dp.add_handler(CallbackQueryHandler(inline_button_list))
     dp.add_handler(cinema_now)
     dp.add_handler(RegexHandler('^(Кинотеатры)$', cinema_location, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Еще фильмов)$', need_more_movies, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Назад)$', back_to_movies, pass_user_data=True))
     
     
     
