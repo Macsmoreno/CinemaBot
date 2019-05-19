9b0c48e2cd8fcebbdc144535d27e7682abad5d2a
Macs Moreno <m.m0ren0@yandex.ru>
2019-05-19 19:23:29 +0300

    Удалены лишние обработчики

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from handlers import greet_user, cinema_list, cinema_location, inline_button_info, inline_button_list, \
-    need_more_movies, back_to_movies
+    need_more_movies, back_to_movies, inline_button_schedule
 
 from telegram.ext import Updater, CommandHandler,  MessageHandler, RegexHandler, ConversationHandler, Filters, \
     CallbackQueryHandler
@@ -15,22 +15,16 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     
-    logging.info('Бот активирован')
-    
-    cinema_now = ConversationHandler(
-        entry_points = [],
-        states = {
-        },
-        fallbacks = []
-    )
 
-    
+    logging.info('Бот активирован')
+
+
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(Сейчас в кино)$', cinema_list, pass_user_data=True))
+    dp.add_handler(CommandHandler("start", greet_user, pass_user_data = True))
+    dp.add_handler(RegexHandler('^(Сейчас в кино)$', cinema_list, pass_user_data = True))
     dp.add_handler(CallbackQueryHandler(inline_button_info))
     dp.add_handler(CallbackQueryHandler(inline_button_list))
-    dp.add_handler(cinema_now)
+    dp.add_handler(CallbackQueryHandler(inline_button_schedule))
     dp.add_handler(RegexHandler('^(Кинотеатры)$', cinema_location, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Еще фильмов)$', need_more_movies, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Назад)$', back_to_movies, pass_user_data=True))
